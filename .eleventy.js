b4fd4e42d55f7bac02ac4f61fd388dc0e9f076f4
Megan Chang <megamegerr@gmail.com>
2024-07-26 16:11:38 -0700

    added tags collections. tags working in nav

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,19 @@ module.exports = function (eleventyConfig) {
     // rev: true
   });
 
+	// Return all the tags used in a collection
+	eleventyConfig.addFilter("getAllTags", collection => {
+		let tagSet = new Set();
+		for(let item of collection) {
+			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+		}
+		return Array.from(tagSet);
+	});
+
+	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+		return (tags || []).filter(tag => ["all", "blog"].indexOf(tag) === -1);
+	});
+
   eleventyConfig.setNunjucksEnvironmentOptions({
     throwOnUndefined: true,
     autoescape: false, // warning: don’t do this!
